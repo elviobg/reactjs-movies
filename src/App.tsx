fcf0af16b964949c9352fe92b998815186f46fea
Elvio Batista <elvio.batista@pebmed.com.br>
2022-01-22 18:29:44 -0300

    limpeza de código

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { useEffect, useState } from 'react';
-
-import { Button } from './components/Button';
-
-
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
-import './styles/sidebar.scss';
 
 interface GenreResponseProps {
   id: number;
